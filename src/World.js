faedd9cecb524b8e2c471bc3e37a23adbb460569
Kai Zhang <kai.zhangxa@gmail.com>
2017-04-11 16:37:41 +0200

    World can init layout

--- src/World.js
+++ src/World.js
@@ -1,5 +1,23 @@
+'use strict';
+
 class World {
-    constructor() {}
+  constructor() {}
+
+  init(x = 5, y = 5) {
+    if (x === 0 && y ===0 ) {
+      this.layout = [[]]
+      return
+    }
+
+    this.layout = []
+    for (var i = 0; i < x; i++) {
+      let row = []
+      for (var j = 0; j < y; j++) {
+        row.push(0)
+      }
+      this.layout.push(row)
+    }
+  }
 };
 
 module.exports = World;
--- tests/WorldTest.js
+++ tests/WorldTest.js
@@ -11,4 +11,43 @@ describe('World', () => {
     });
   });
 
+  describe('instance', () => {
+    it('should be able to init a rectangle layout', () => {
+      let world = new World();
+      world.init(0, 0);
+      assert.deepEqual([[]], world.layout);
+    });
+  });
+
+  describe('instance', () => {
+    it('should be able to init a rectangle layout without init params 5*5', () => {
+      let world = new World();
+      world.init();
+      assert.deepEqual(
+        [
+          [0,0,0,0,0],
+          [0,0,0,0,0],
+          [0,0,0,0,0],
+          [0,0,0,0,0],
+          [0,0,0,0,0]
+        ],
+        world.layout
+      );
+    })
+  })
+
+  describe('instance', () => {
+    it('should be able to init a rectangle layout without init params 3*3', () => {
+      let world = new World();
+      world.init(3, 3);
+      assert.deepEqual(
+        [
+          [0,0,0],
+          [0,0,0],
+          [0,0,0]
+        ],
+        world.layout
+      );
+    })
+  })
 });
